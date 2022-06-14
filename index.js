fdcbed750fabaa84ed48737eceb4f007e2462714
vinay kumar <tgvinay.7@gmail.com>
2022-06-14 23:53:01 +0530

    cors fix

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require('cors');
 const postgress = require("./services/postgress_service");
 const router = require("./routes/routes");
 var app = express();
 
-
+app.use(cors({
+    origin: 'https://scclvts.web.app'
+}));
 app.use(router);
 
 app.listen(process.env.PORT, () => {
