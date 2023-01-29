a2ef7bccec3834d2eacc30aba2d86aee78061e22
vinay kumar <tgvinay.7@gmail.com>
2023-01-29 23:48:15 +0530

    dev2 comm

--- controllers/controller.js
+++ controllers/controller.js
@@ -417,7 +417,12 @@ exports.getVehicleRouteRfidPoint = async (req, res, next) => {
                                                     const data = await postgress.updateTripDetail(currentPoint.trip_info_id, "AUTO", new Date().toISOString(), true, req.body.front_view, req.body.top_view)
                                                     res.send(true);
                                                 } else {
-                                                    res.send(false);
+                                                    if(previousPoint.optional === true && previousPoint.status === false && currentPoint.optional === false && currentPoint.status === false){
+                                                        const data = await postgress.updateTripDetail(currentPoint.trip_info_id, "AUTO", new Date().toISOString(), true, req.body.front_view, req.body.top_view)
+                                                        res.send(true);
+                                                       } else {
+                                                        res.send(false);
+                                                       }
                                                 }
                                             }
 
